67507198dd4c1760e3ac3242d7ac67d99d4417e7
ushapatturaj <ushadevipatturaj@gmail.com>
2020-08-12 11:04:16 +0200

    Division by zero

---
+++ Problems/Zero/task.py
@@ -0,0 +1,8 @@
+n = int(input())
+denominator = int(input())
+try:
+    result = n // denominator
+except ZeroDivisionError:
+    print("Division by zero is not supported")
+else:
+    print(result)
