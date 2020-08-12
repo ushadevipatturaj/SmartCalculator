e9612a1e69034338b6c89812a12691a6bb1e5841
ushapatturaj <ushadevipatturaj@gmail.com>
2020-08-12 10:49:35 +0200

    Try except else and finally blocks

---
+++ Problems/Finalizing the flow/task.py
@@ -0,0 +1,8 @@
+try:
+    name, surname = input().split()
+except Exception:
+    print("You need to enter exactly 2 words. Try again!")
+else:
+    print(f"Welcome to our party, {name} {surname}")
+finally:
+    print("You will be more than welcome!")
